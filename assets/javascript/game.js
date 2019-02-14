9f8364797850703a1a53c5308a5191552e8eea6b
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-02-13 21:37:40 -0600

    Added file structure, index.html basic html outline and link to js file, game play pseudo code written in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// declare variables
+
+// initialize game with press key event to start game
+
+// computer picks a random word from an array and displays each character as an underscore
+
+// computer "listens" for the player keystroke input
+
+// computer compares the player input to letters in the word and in the already guessed letters
+    // if the letter is included in the word the letter is "revealed" and replaces the "underscore" placeholder and added to the "already guessed" letters list
+    // if the letter is NOT included in the word the letter is added to the "already guessed" letters list and reduces the number of wrong letter available guesses by one
+    
+    // if all letters have been correctly guessed and revealed, update the wins by one and alert "you've won"
+    // if wrong letter available guesses value is zero alert "you loose" and update the "losses" by one
+
+// after alert cleared, prompt "play again?"
+    // if 'yes', computer picks new random word, resets the number of wrong letter guesses to the default value, clears the "already guessed" letters list
+
